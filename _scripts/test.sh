99f80817f4e6db77d910c49e1d4ef50821437411
Joe Flasher <jflasher@amazon.com>
2019-04-01 11:36:18 -0400

    Ignoring YAML loading warnings

--- _scripts/test.sh
+++ _scripts/test.sh
@@ -3,6 +3,9 @@
 # Make sure we exit(1) if there is an error
 set -e
 
+# For our use case, we can ignore loading warnings
+PYTHONWARNINGS=ignore::yaml.YAMLLoadWarning
+
 for f in datasets/*
 do
   echo $f
