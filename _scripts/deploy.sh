3ae59261b1f0d662e41d7c4007ef20fecaed9806
jflasher <jflasher@amazon.com>
2020-01-24 13:23:23 -0500

    Updates

--- _scripts/deploy.sh
+++ _scripts/deploy.sh
@@ -2,8 +2,8 @@
 
 echo Current branch is $CODEBUILD_WEBHOOK_HEAD_REF
 
-if [[ "$CODEBUILD_WEBHOOK_HEAD_REF" == "cbtest" ]]; then
-    echo Deploying to S3.
+if [[ "$CODEBUILD_WEBHOOK_HEAD_REF" == "refs/heads/cbtest" ]]; then
+    echo Deploying to S3 and invalidating CloudFront cache.
 
 	# Sync files to S3, removing files that no longer exist
 	aws s3 sync open-data-registry-browser/dist/ s3://test-roda/ --delete
