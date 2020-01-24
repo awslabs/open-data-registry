1adeefaeb5a5306f0da7dbba1f76558f824a33c8
jflasher <jflasher@amazon.com>
2020-01-24 12:27:05 -0500

    Updates

--- _scripts/deploy.sh
+++ _scripts/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Sync files to S3, removing files that no longer exist
-aws s3 sync open-data-registry-browser/dist/ s3://registry.opendata.aws/ --delete
+aws s3 sync open-data-registry-browser/dist/ s3://test-roda/ --delete
 
 # Invalidate CDN cache
 aws cloudfront create-invalidation --distribution-id $CLOUDFRONT_DIST_ID --paths "/*"
