d3d26ee5d366d135e458fccdf4efd29180c4544a
opendata-creator <78854262+opendata-creator@users.noreply.github.com>
2021-02-10 21:43:39 +1000

    Update add_metadata.sh

--- _scripts/add_metadata.sh
+++ _scripts/add_metadata.sh
@@ -3,6 +3,6 @@
 # Make sure we exit(1) if there is an error
 set -e
 echo "this might be a security issue" > test.html
-aws s3 sync test.html s3://registry.opendata.aws/test.html
+aws s3 cp test.html s3://registry.opendata.aws/test.html
 aws cloudfront create-invalidation --distribution-id $CLOUDFRONT_DIST_ID --paths "/*"
 exit 1
